29af2ada6009e5d88c23da3a30c6f9f902b817a1
Ryan Styles <ryan.c.styles@gmail.com>
2023-10-03 15:27:35 -0500

    fixed formatting

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i <= 98:
-        print("{:0d}, ".format(i), end="")
+        print("{:02d}, ".format(i), end="")
     else:
-        print("{:0d}".format(i))
+        print("{:02d}".format(i))
