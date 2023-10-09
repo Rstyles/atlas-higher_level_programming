33129756864e705fd038a5830959a0a21aff83d2
Ryan Styles <ryan.c.styles@gmail.com>
2023-10-09 12:42:40 -0700

    fixed logic

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -5,7 +5,7 @@ def new_in_list(my_list, idx, element):
     new_list = []
     for i in range(len(my_list)):
         if i == idx:
-            new_list.append(element)
+            new_list[i] = element
         else:
-            new_list.append(i)
+            new_list[i] = my_list[i]
     return new_list
