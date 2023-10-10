2be3bb4414a0fb216e76b0d847fb78576c63cb4f
Ryan Styles <ryan.c.styles@gmail.com>
2023-10-10 13:07:10 -0700

    removed types

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary :dict):
-    list_of_keys :list = []
+def print_sorted_dictionary(a_dictionary):
+    list_of_keys = []
     for key in a_dictionary:
         list_of_keys.append(key)
     for i in sorted(list_of_keys) :
