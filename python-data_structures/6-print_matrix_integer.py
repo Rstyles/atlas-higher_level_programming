d4b443d3d2b21c3cf00610f203cf2d34d400f401
Ryan Styles <ryan.c.styles@gmail.com>
2023-10-10 07:43:57 -0700

    changed print format

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,9 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            if j != len(matrix[i]) - 1:
+            if j == 0:
+                print("{:d}".format(matrix[i][j]), end="")
+            elif j != len(matrix[i]) - 1:
                 print(" {:d}".format(matrix[i][j]), end="")
             else:
                 print(" {:d}".format(matrix[i][j]))
