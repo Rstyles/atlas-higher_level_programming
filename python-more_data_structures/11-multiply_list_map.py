053bb895683d792e7b41c04ff98421c35f9645f6
Ryan Styles <ryan.c.styles@gmail.com>
2023-10-10 14:06:17 -0700

    refactored to less number of lines

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    results =  map(lambda mult: mult * number, my_list)
-    return list(results)
+    return list(map(lambda mult: mult * number, my_list))
