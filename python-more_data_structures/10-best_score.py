d3b3c4a79c079d6a3f2093673c983e6c8038b748
Ryan Styles <ryan.c.styles@gmail.com>
2023-10-10 13:50:51 -0700

    added files for 10

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def best_score(a_dictionary: dict):
+    biggest: str = None
+    if a_dictionary != None:
+        for key in a_dictionary:
+            if biggest == None or a_dictionary[key] > a_dictionary[biggest]:
+                biggest = key
+    return biggest
