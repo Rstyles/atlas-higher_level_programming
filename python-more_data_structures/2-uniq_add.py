257945918803bfbbf5a1b534a07c57131de7db61
Ryan Styles <ryan.c.styles@gmail.com>
2023-10-10 12:03:59 -0700

    formatting

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -6,10 +6,10 @@ def uniq_add(my_list=[]):
         sum += i
     return sum
 
+
 def get_uniqs(my_list=[]):
     uniq_list = []
     for i in range(len(my_list)):
         if my_list[i] not in uniq_list:
             uniq_list.append(my_list[i])
     return uniq_list
-
