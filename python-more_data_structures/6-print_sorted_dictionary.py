62b19d25af3513d7b1f255e5efc7f9f30c74b5f1
Ryan Styles <ryan.c.styles@gmail.com>
2023-10-10 13:08:36 -0700

    formatting whitespace

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,5 +3,5 @@ def print_sorted_dictionary(a_dictionary):
     list_of_keys = []
     for key in a_dictionary:
         list_of_keys.append(key)
-    for i in sorted(list_of_keys) :
+    for i in sorted(list_of_keys):
         print(f"{i}: {a_dictionary[i]}")
