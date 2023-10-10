710a9eaec5a7c1d4e8fa79cbe38f77f06a0960b9
Ryan Styles <ryan.c.styles@gmail.com>
2023-10-10 13:05:26 -0700

    formatting

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary:dict):
-    list_of_keys:list = []
+def print_sorted_dictionary(a_dictionary: dict):
+    list_of_keys: list = []
     for key in a_dictionary:
         list_of_keys.append(key)
-    for i in sorted(list_of_keys):
+    for i in sorted(list_of_keys) :
         print(f"{i}: {a_dictionary[i]}")
