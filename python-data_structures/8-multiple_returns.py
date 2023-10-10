6efc5d4b74324cb696001063cd5fe82c972cc213
Ryan Styles <ryan.c.styles@gmail.com>
2023-10-10 09:02:59 -0700

    fixed starting values

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = None
-    first_Char = ''
+    length = 0
+    first_Char = 'None'
     if len(sentence) > 0:
         length = len(sentence)
         first_Char = sentence[0]
