8f7072a6dfd84ff31432ddf281a9b35a0f1745e8
Ryan Styles <ryan.c.styles@gmail.com>
2023-10-10 13:51:55 -0700

    fixed Pycodestyle validation

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary: dict):
     biggest: str = None
-    if a_dictionary != None:
+    if a_dictionary is not None:
         for key in a_dictionary:
             if biggest == None or a_dictionary[key] > a_dictionary[biggest]:
                 biggest = key
