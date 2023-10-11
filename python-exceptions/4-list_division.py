21181777e5543b136b0bec6f8ec89cd90ae97558
Ryan Styles <ryan.c.styles@gmail.com>
2023-10-11 15:55:28 -0500

    added files for 4

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length: int):
+    output = []
+    for i in range(list_length):
+        try:
+            result = my_list_1[i] / my_list_2[i]
+        except ArithmeticError:
+            result = 0
+            print("division by 0")
+        except IndexError:
+            print("out of range")
+        except TypeError:
+            print("wrong type")
+        finally:
+            output.append(result)
+    return output
