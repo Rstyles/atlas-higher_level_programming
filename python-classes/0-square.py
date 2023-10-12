12317dbf913e39986172b4ab0fe743674e5ea29e
Ryan Styles <ryan.c.styles@gmail.com>
2023-10-12 09:32:18 -0500

    moved docstring

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+"""This is an empty class that defines a square"""
+
 class Square:
-    """This is an empty class that defines a square"""
     def __init__(self) -> None:
         pass
