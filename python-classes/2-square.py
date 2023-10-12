e9a87ba6f9dbb908b389177a6ad97ee73dad388b
Ryan Styles <ryan.c.styles@gmail.com>
2023-10-12 13:50:19 -0500

    added files for 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This Module contians the class Square"""
+
+
+class Square:
+    """Defines a Square with a size"""
+
+    def __init__(self, size=0):
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        elif not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        self.__size = size
