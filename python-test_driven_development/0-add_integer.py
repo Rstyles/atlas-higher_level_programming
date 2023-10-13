3dd6a52aeeb4439425086ad6c412e8808c6e0882
Ryan Styles <ryan.c.styles@gmail.com>
2023-10-13 15:41:06 -0500

    added doc comments

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""add_integer(a, b)
+Add 2 numbers together
+"""
+
+
 def add_integer(a, b=98):
+    """_Adds a and b together
+    """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
