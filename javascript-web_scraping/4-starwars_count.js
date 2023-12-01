932eb2228fc99915ad12b80d1dca77cc9aa75a0f
Ryan Styles <ryan.c.styles@gmail.com>
2023-12-01 13:24:59 -0600

    added file for 4

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const response = JSON.parse(body);
+    let wedgeAntillesCount = 0;
+    response.results.forEach(element => {
+      if (element.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+        wedgeAntillesCount++;
+      }
+    });
+    console.log(wedgeAntillesCount);
+  }
+});
