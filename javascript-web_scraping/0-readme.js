4e127955998b12af9388be045c43691016a6af65
Ryan Styles <ryan.c.styles@gmail.com>
2023-12-01 09:48:26 -0600

    Added files for task 0

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node/
+
+const fs = require("fs");
+
+const args = process.argv.slice(2);
+
+try {
+  const content = fs.readFileSync(args[0], "utf8");
+  console.log(content);
+} catch (error) {
+  console.log(error);
+}
