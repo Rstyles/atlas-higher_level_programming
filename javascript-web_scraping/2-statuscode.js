7d78cd4f6d51eb28273df4f7f39aeb4cdf7de1fa
Ryan Styles <ryan.c.styles@gmail.com>
2023-12-01 11:07:54 -0600

    semistandard

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,9 +3,9 @@
 const request = require('request');
 
 request.get(process.argv[2], (error, response) => {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('code:', response.statusCode);
-    }
-})
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code:', response.statusCode);
+  }
+});
