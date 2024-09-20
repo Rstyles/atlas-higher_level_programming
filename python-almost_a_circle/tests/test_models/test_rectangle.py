7e22c394a76d2c3e378f63caf9b82a7975a3f83d
Ryan Styles <ryan.c.styles@gmail.com>
2024-09-20 15:56:39 -0500

    Refactor test_rectangle.py: Update save_to_file method test

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -169,9 +169,8 @@ class TestRectangle(unittest.TestCase):
         """
         Test save_to_file method with empty list
         """
-        Rectangle.save_to_file([])
-        with open("Rectangle.json", "r") as file:
-            self.assertEqual(file.read(), "[]")
+        r = Rectangle(1,1)
+        self.assertEqual(r.save_to_file([]), None)
 
     def test_save_to_file_None(self):
         """
